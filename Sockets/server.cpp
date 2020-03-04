9055583e18aeb1297af3d4a1c6d5174e2cc8a6a6
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-04 10:59:04 -0500

    Update server.cpp

--- Sockets/server.cpp
+++ Sockets/server.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 
 int main()
 {
